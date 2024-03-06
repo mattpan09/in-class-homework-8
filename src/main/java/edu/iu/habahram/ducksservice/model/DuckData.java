4e21eb31b373c56c763ad3333922a2499a6aabe1
mppan <mppan@iu.edu>
2024-03-06 09:17:26 -0500

    Pushing inclass homework 8

--- src/main/java/edu/iu/habahram/ducksservice/model/DuckData.java
+++ src/main/java/edu/iu/habahram/ducksservice/model/DuckData.java
@@ -1,6 +1,6 @@
 package edu.iu.habahram.ducksservice.model;
 
-public record DuckData(int id, String type) {
+public record DuckData(int id, String type) implements Comparable<DuckData> {
 
     public String toLine() {
         return String.format("%1$s,%2$s", id(), type());
@@ -9,6 +9,17 @@ public record DuckData(int id, String type) {
     public static DuckData fromLine(String line) {
         String[] tokens = line.split(",");
         return new DuckData(Integer.parseInt(tokens[0]), tokens[1]);
+    }
+
+    @Override
+    public int compareTo(DuckData o) {
+        //Compare the types
+        int typeComparison = type.compareTo(o.type);
+        if(typeComparison != 0) {
+            return typeComparison;
+        }
+        //If the types are the same, compare the ids
+        return Integer.compare(id, o.id);
     }
 
 }
